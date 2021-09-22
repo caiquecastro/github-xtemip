f06dd8dde5d386bd4d79e5ecc2a04620881cd622
caiquecastro <castro.caique@gmail.com>
2021-09-22 16:30:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-xtemip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-xtemip)
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react';
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons';
+
+import { Hero } from '../components/Hero';
+import { Container } from '../components/Container';
+import { Main } from '../components/Main';
+import { DarkModeSwitch } from '../components/DarkModeSwitch';
+import { CTA } from '../components/CTA';
+import { Footer } from '../components/Footer';
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Next ❤️ Chakra</Text>
+    </Footer>
+    <CTA />
+  </Container>
+);
+
+export default Index;
